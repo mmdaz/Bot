795ece81db445be2a044bc30c66bbd3dc30e9a1d
mmdni <ma313@aut.ac.ir>
2018-08-19 02:20:39 +0430

    The simple bot created but has amazing bug :|

--- Muhammad's_Bot.py
+++ Muhammad's_Bot.py
@@ -1,11 +1,11 @@
+from time import sleep
+
 import requests
 
 url = "https://api.telegram.org/bot699267489:AAFNulySbda5Ik9FKtHzLGpaUzwOim6Ut3k/"
 
 def get_updates_json(request):
     response = requests.get(request + 'getUpdates')
-    # print(response.json())
-    # print("salam")
     return response.json()
 
 def last_update(data):
@@ -13,7 +13,7 @@ def last_update(data):
     total_updates = len(results) - 1
     return results[total_updates]
 
-# get_updates_json(url)
+print(last_update(get_updates_json(url)))
 
 def get_chat_id(update):
     chat_id = update['message']['chat']['id']
@@ -24,6 +24,32 @@ def send_mess(chat, text):
     response = requests.post(url + 'sendMessage', data=params)
     return response
 
-chat_id = get_chat_id(last_update(get_updates_json(url)))
+def get_user_name(update):
+    user_specifications = update['message']['from']['username']
+    return user_specifications
 
-send_mess(chat_id, 'salam')
+
+def main():
+
+    update_id = last_update(get_updates_json(url))['update_id']
+
+    while True:
+
+        print(update_id)
+        print(last_update(get_updates_json(url))['update_id'])
+        print(get_user_name(last_update(get_updates_json(url))))
+        if get_user_name(last_update(get_updates_json(url))) == 'mhmd_azhdari' and update_id == last_update(get_updates_json(url))['update_id']:
+            send_mess(get_chat_id(last_update(get_updates_json(url))), 'hello muhammad')
+            print("salam")
+            update_id = str(int(update_id) + 1)
+        elif get_user_name(last_update(get_updates_json(url))) == 'Maryamsf19' and update_id == last_update(get_updates_json(url))['update_id']:
+            send_mess(get_chat_id(last_update(get_updates_json(url))), 'Developer of this bot loves you')
+            update_id = str(int(update_id) + 1)
+        elif update_id == last_update(get_updates_json(url))['update_id']:
+            send_mess(get_chat_id(last_update(get_updates_json(url))), 'Hello')
+            update_id = str(int(update_id) + 1)
+
+        sleep(3)
+
+if __name__ == '__main__':
+    main()
